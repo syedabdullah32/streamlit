7c5313a63b7e9f2ff45c4980a79f4c98a3ce1728
Syed Meer Abdullah <126704055+syedabdullah32@users.noreply.github.com>
2023-06-01 13:25:57 +0500

    Update and rename app1.py to app2.py

--- app2.py
+++ app2.py
@@ -7,7 +7,7 @@ def main():
     st.title("My Web Scraper")
 
     # Get the URL from the user
-    url = st.text_input("Enter the URL", "https://www.whatmobile.com.pk/")
+    url = st.text_input("Enter the URL", "https://quotes.toscrape.com/")
 
     # Make a GET request to the website
     response = requests.get(url)
@@ -15,12 +15,12 @@ def main():
     # Parse the HTML content using BeautifulSoup
     soup = BeautifulSoup(response.content, 'html.parser')
 
-    # Extract the names using regular expressions
-    names = soup.find_all('a', class_='aname')
+    # Extract the tags using regular expressions
+    tags = soup.find_all('a')
 
-    # Display the extracted names in Streamlit
-    for name in names:
-        st.write(name.text.strip())
+    # Display the extracted tags in Streamlit
+    for tag in tags:
+        st.write(tag.text.strip())
 
 if __name__ == '__main__':
     main()
