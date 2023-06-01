4079c6894e7cb688f6e9236471473e6ef08a9939
Syed Meer Abdullah <126704055+syedabdullah32@users.noreply.github.com>
2023-06-01 10:38:51 +0500

    app.py

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
 import requests
 from bs4 import BeautifulSoup
 
-# URL of the website to scrape
-url = "https://www.goodreads.com/"  # Replace with the actual website URL
+# Get user input for the website URL
+url = input("Enter the website URL: ")
+
+# Get user input for the HTML element and class to scrape
+element_tag = input("Enter the HTML element tag (e.g., h2, p, div): ")
+element_class = input("Enter the HTML element class (optional, press Enter if none): ")
 
 # Send a GET request to the website
 response = requests.get(url)
@@ -12,14 +16,18 @@ if response.status_code == 200:
     # Parse the HTML content using BeautifulSoup
     soup = BeautifulSoup(response.content, "html.parser")
 
-    # Find all elements with the book titles
-    book_title_elements = soup.find_all("h2", class_="book-title")  # Replace with the actual HTML element and class
+    # Find all elements with the specified tag and class
+    if element_class:
+        elements = soup.find_all(element_tag, class_=element_class)
+    else:
+        elements = soup.find_all(element_tag)
 
-    # Extract the book titles from the elements
-    book_titles = [element.text.strip() for element in book_title_elements]
+    # Extract the text from the elements
+    scraped_data = [element.text.strip() for element in elements]
 
-    # Print the book titles
-    for title in book_titles:
-        print(title)
+    # Print the scraped data
+    print("Scraped Data:")
+    for data in scraped_data:
+        print(data)
 else:
     print("Failed to retrieve the webpage. Status code:", response.status_code)
